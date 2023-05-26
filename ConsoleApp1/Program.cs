f3e96f6764c2da2b3309cce27de7ff0c83efd956
maxaiette1129 <maxaiettec@gmail.com>
2023-05-26 20:31:23 +0800

    Add project files.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,31 @@
+﻿namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            using (StreamWriter writer = new StreamWriter("path/to/file.txt"))
+            {
+
+                string name = "Maxaiette";
+                int id = 1231241254;
+                writer.WriteLine($"Hello, World! {name} {id}");
+
+                writer.WriteLine("This is an example of StreamWriter.");
+
+                // You can also use the Write method to write without a line terminator
+                writer.Write("This ");
+                writer.Write("is ");
+                writer.Write("a ");
+                writer.Write("single ");
+                writer.Write("line.");
+
+                // Flush the buffer to ensure all data is written to the file
+                writer.Flush();
+
+                // Close the StreamWriter (this will also flush any remaining data)
+                writer.Close();
+            }
+        }
+    }
+}
